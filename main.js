4ae5ecdd089fe594822a86338e55574c7d3c7661
Akram Eldamaty <akram_eldamaty@hotmail.com>
2018-01-09 00:27:12 -0500

    fix JS and update looks
    updated

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //example use of the api
 
 function apiCall(){
-  $.getJSON('https://www.omdbapi.com/?apikey=c4fc25b5&t=' + encodeURI("Game of thrones")).then(function(response){
+  $.getJSON('https://www.omdbapi.com/?apikey=c4fc25b5&t=' + encodeURI("Prison Break")).then(function(response){
   console.log(response.totalSeasons);
 })
 }
