192a24894a83707ef01cfa6180995970a9f90bac
Abdul <abdulelrahwan@gmail.com>
2018-01-08 21:59:32 -0500

    ex

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//example use of the api
+
 function apiCall(){
   $.getJSON('https://www.omdbapi.com/?apikey=c4fc25b5&t=' + encodeURI("Game of thrones")).then(function(response){
   console.log(response.totalSeasons);
